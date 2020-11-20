8b2b04cf96b098f4bbf3d3da8c1b4ace30f452eb
Artem Chepak <artemchepakwebdeveloper@users.noreply.github.com>
2020-11-20 12:39:49 +0200

    feat: add greetings section styles

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,15 @@ $(function() {
     //sticky header menu
     let sticky = new Sticky('.sticky');
     sticky.update();
+
+    //slider
+    $('.greetings__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: false,
+    });
 });
