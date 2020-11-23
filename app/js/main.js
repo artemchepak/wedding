1e57457d161d03d16d46105e526a4d539b1fbdbd
Artem Chepak <artemchepakwebdeveloper@users.noreply.github.com>
2020-11-23 14:11:31 +0200

    feat: add map

--- app/js/main.js
+++ app/js/main.js
@@ -13,4 +13,26 @@ $(function() {
         autoplaySpeed: 2000,
         pauseOnHover: false,
     });
+
+
 });
+//map
+function myMap() {
+    let mapCanvas = document.getElementById("map");
+    let mapOptions = {
+        center: new google.maps.LatLng(49.9860637681822, 36.22593443398573),
+        zoom: 18
+    };
+    let map = new google.maps.Map(mapCanvas, mapOptions);
+    let marker = new google.maps.Marker({
+        position: new google.maps.LatLng(49.9860637681822, 36.22593443398573),
+        map: map,
+        icon: '../images/icons/map.svg'
+    });
+    let infoWindow = new google.maps.InfoWindow({
+        content: '<b>Место проведения основной церемонии</b>'
+    });
+    marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+    });
+}
